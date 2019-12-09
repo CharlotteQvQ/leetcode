13365ffd494d1cecc7bdc02061e3e3751d421f7e
Charlotte Li <charlotte_ljh@163.com>
2019-12-09 23:49:57 +0800

    format code

--- src/com/charlotte/algorithm/LeetCode0136.java
+++ src/com/charlotte/algorithm/LeetCode0136.java
@@ -26,7 +26,7 @@ package com.charlotte.algorithm;
 public class LeetCode0136 {
 
     public static void main(String[] args) {
-        int[] nums = {4,1,2,1,2};
+        int[] nums = {4, 1, 2, 1, 2};
         int result = singleNumber(nums);
         System.out.println(result);
     }
