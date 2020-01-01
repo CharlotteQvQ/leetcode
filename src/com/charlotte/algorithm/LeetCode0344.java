2373610bee489ecc9c990e9c6ab90f55073ec1e6
Charlotte Li <charlotte_ljh@163.com>
2020-01-01 22:10:35 +0800

    I'm noob.

---
+++ src/com/charlotte/algorithm/LeetCode0344.java
@@ -0,0 +1,41 @@
+package com.charlotte.algorithm;
+
+/**
+ * @Author: Charlotte Li
+ * @Date: 2020/1/1 21:53
+ * @Description: Write a function that reverses a string. The input string is given as an array of characters char[].
+ * <p>
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * <p>
+ * You may assume all the characters consist of printable ascii characters.
+ * <p>
+ *  
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: ["h","e","l","l","o"]
+ * Output: ["o","l","l","e","h"]
+ * Example 2:
+ * <p>
+ * Input: ["H","a","n","n","a","h"]
+ * Output: ["h","a","n","n","a","H"]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @version: 1.0
+ */
+public class LeetCode0344 {
+    public static void main(String[] args) {
+        char[] s = {};
+        reverseString(s);
+    }
+
+    public static void reverseString(char[] s) {
+        for (int i = 0; i < s.length / 2; i++) {
+            char temp = s[s.length - i - 1];
+            s[s.length - i - 1] = s[i];
+            s[i] = temp;
+        }
+    }
+}
